5e52ce3624eaa15ee0e1a1cd8fd5b3a4283820ff
Çağatay Çelimli <c.celimli@live.co.uk>
2024-01-05 00:31:08 +0300

    Caching

---
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching;
+
+public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ICacheableRequest
+{
+    private readonly CacheSettings _cacheSetting;
+    private readonly IDistributedCache _cache;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (request.BypassCache)
+        {
+            return await next();
+        }
+
+        TResponse response;
+        byte[] cachedResponse = await _cache.GetAsync(request.CacheKey, cancellationToken);
+        if (cachedResponse != null)
+        {
+            response=JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse));
+        }
+        else
+        {
+            response = await getResponseAndAddToCache(request, next, cancellationToken);
+        }
+
+        return response;
+    }
+
+    private async Task<TResponse?> getResponseAndAddToCache(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        TResponse response = await next();
+
+        TimeSpan slidingExpiration = request.SlidingExpiration ?? TimeSpan.FromDays(_cacheSetting.SlidingExpiration);
+        DistributedCacheEntryOptions cacheOptions = new() { SlidingExpiration = slidingExpiration };
+
+        byte[] serializeData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+
+        await _cache.SetAsync(request.CacheKey, serializeData, cacheOptions, cancellationToken);
+
+        return response;
+
+    }
+}
