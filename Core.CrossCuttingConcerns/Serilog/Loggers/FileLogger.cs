902fa5c4169f2fdd423e383a9a727656a88b095f
Çağatay Çelimli <c.celimli@live.co.uk>
2024-01-06 04:31:36 +0300

    Exception Logging

--- Core.CrossCuttingConcerns/Serilog/Loggers/FileLogger.cs
+++ Core.CrossCuttingConcerns/Serilog/Loggers/FileLogger.cs
@@ -19,7 +19,7 @@ namespace Core.CrossCuttingConcerns.Serilog.Loggers
             _configuration = configuration;
 
             FileLogConfiguration logConfig = 
-                configuration.GetSection("SerilogLogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>()
+                configuration.GetSection("SeriLogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>()
                 ?? throw new Exception(SerilogMessages.NullOptionsMessage);
 
             string logFilePath = string.Format(format: "{0}{1}", arg0: Directory.GetCurrentDirectory() + logConfig.FolderPath, arg1: ".txt");
@@ -30,7 +30,6 @@ namespace Core.CrossCuttingConcerns.Serilog.Loggers
                 fileSizeLimitBytes: 5000000,
                 outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}"
                 ).CreateLogger();
-
         }
     }
 }
